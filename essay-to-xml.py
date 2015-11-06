55c2ee20fbdd5cd32fc5cd9b39d3b43f7a5038d9
mhkuu <M.H.vanderKlis@uu.nl>
2015-11-06 16:05:12 +0100

    Added correction features (which are invalid in FoLiA)

--- essay-to-xml.py
+++ essay-to-xml.py
@@ -9,6 +9,11 @@ GREEDY_MATCH_TAG = re.compile(r'\[(.*)\](\w[\*\w]*)')
 NON_GREEDY_MATCH_TAG = re.compile(r'\[(.*?)\](\w[\*\w]*)')
 
 
+class MyCorrection(folia.Correction):
+    """Allow Corrections to have second-order annotation"""
+    ACCEPTED_DATA = folia.Correction.ACCEPTED_DATA + (folia.Feature,)
+
+
 class PartAnnotation():
     def __init__(self, part, annotation):
         self.split_part(part)
@@ -33,10 +38,10 @@ class PartAnnotation():
             self.problem = a[1] if len(a) >= 2 else None
             self.pos = a[2] if len(a) == 3 else None
         else:
-            self.annotation = ''
-            self.unit = ''
-            self.problem = ''
-            self.pos = ''
+            self.annotation = None
+            self.unit = None
+            self.problem = None
+            self.pos = None
 
     def add_child(self, child, start, end):
         self.children.append(child)
@@ -44,28 +49,52 @@ class PartAnnotation():
         child.start = start
         child.end = end
 
+    def to_folia_sentence_child(self, doc, sentence):
+        original = tokenize(self.original)
+        edited = tokenize(self.edited)
+
+        correction = None
+        # If we're dealing with single words (e.g. spelling errors), create the correction directly on the word.
+        if len(original) == 1 and len(edited) == 1:
+            word = sentence.add(folia.Word)
+            correction = word.add(MyCorrection, folia.New(doc, self.edited), folia.Original(doc, self.original), cls=self.unit)
+        # We are dealing with more than one word, or an insertion/deletion. Create word elements for each token.
+        else:
+            new = folia.New(doc)
+            orig = folia.Original(doc)
+            for w in edited:
+                new.add(folia.Word, w, generate_id_in=sentence)
+            for w in original:
+                orig.add(folia.Word, w, generate_id_in=sentence)
+
+            correction = sentence.add(MyCorrection, new, orig, cls=self.unit)
+
+        if self.problem:
+            correction.add(folia.Feature, subset='problem', cls=self.problem)
+        if self.pos:
+            correction.add(folia.Feature, subset='pos', cls=self.pos)
+
     def to_folia_sentence(self, doc, paragraph):
         sentence = paragraph.add(folia.Sentence)
         parts = []
+        # TODO: order the children by start position
+        # TODO: deal with nesting here.
         for c in self.children:
             parts.append((c.start, c.end, c))
-        if not parts:
-            parts.append((len(self.original), len(self.original), None))
+        parts.append((len(self.original), None, None))
 
         current_position = 0
-        for p in parts:
-            print p[0]
-            tokens = tokenize(self.original[current_position:p[0]])
+        for start, end, child in parts:
+            tokens = tokenize(self.original[current_position:start])
             for token in tokens:
                 sentence.add(folia.Word, token)
-            if p[2]:
-                word = sentence.add(folia.Word)
-                word.add(folia.Correction, folia.New(doc, p[2].edited), folia.Original(doc, p[2].original), cls=p[2].unit)
+            if child:
+                child.to_folia_sentence_child(doc, sentence)
+            current_position = end
 
-            current_position = p[1]
-        tokens = tokenize(self.original[current_position:])
-        for token in tokens:
-            sentence.add(folia.Word, token)
+        if self.unit:
+            layer = sentence.add(folia.SemanticRolesLayer)
+            layer.add(folia.SemanticRole(doc, cls=self.unit))
 
     def __str__(self):
         result = '{} <{}>'.format(self.original, self.unit)
@@ -77,7 +106,9 @@ class PartAnnotation():
 
 
 def match_outer(s):
-    match = re.search(GREEDY_MATCH_TAG, s)
+    # TODO: this does not work:
+    # [Zwarte pieten/Zwarte Pieten]HN*O zijn zwart als [roed/roet]SPE*I*N [/.]L*O
+    match = GREEDY_MATCH_TAG.search(s)
     if match:
         sentence = match.group(1)
         annotation = match.group(2)
@@ -89,22 +120,23 @@ def match_outer(s):
 
 
 def match_inner(s, pa=None):
-    matches = re.finditer(NON_GREEDY_MATCH_TAG, s)
+    matches = NON_GREEDY_MATCH_TAG.finditer(s)
     if not pa:
         pa = PartAnnotation(s, None)
     for match in matches:
-        #print 'match', match.group(1), match.group(2)
-        if match.start(0) == 0:
+        # If we match the whole sentence, create a new annotation
+        if match.group(0) == s:
             sentence = match.group(1)
             annotation = match.group(2)
             pa = PartAnnotation(sentence, annotation)
+        # Otherwise, add the matches to the existing annotation
         else:
             pa.add_child(PartAnnotation(match.group(1), match.group(2)), match.start(0), match.end(0))
     return pa
 
 
 def extract_corrections(s, pa=None):
-    if re.search(HAS_INNER, s):
+    if HAS_INNER.search(s):
         if pa is not None:
             print 'loop in loop! bug de bug'
         else:
@@ -121,6 +153,7 @@ def count_brackets(line):
 def process_file():
     doc = folia.Document(id='example')
     doc.declare(folia.Correction, 'https://raw.githubusercontent.com/UiL-OTS-labs/essay-annotation/config/corrections.foliaset.xml')
+    doc.declare(folia.SemanticRolesLayer, 'https://raw.githubusercontent.com/UiL-OTS-labs/essay-annotation/config/semanticroles.foliaset.xml')
     text = doc.append(folia.Text)
     paragraph = text.add(folia.Paragraph)
     with codecs.open('data/T1_VOWY0Q147044.txt', 'rb') as f:
@@ -129,7 +162,7 @@ def process_file():
             if count_brackets(line):
                 pa = extract_corrections(line)
                 pa.to_folia_sentence(doc, paragraph)
-                print pa
+                #print pa
             else:
                 print 'Number of brackets does not match on line', n
     doc.save('data/out.txt')
