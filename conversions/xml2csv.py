be0fdeee7e96499891d7ab2f5169408c9e599acc
mhkuu <M.H.vanderKlis@uu.nl>
2016-02-05 13:47:48 +0100

    Refactoring

--- conversions/xml2csv.py
+++ conversions/xml2csv.py
@@ -1,3 +1,6 @@
+import glob
+import os
+
 from pynlpl.formats import folia
 
 from utils import UnicodeWriter
@@ -16,30 +19,41 @@ def get_feature(element, subset):
         print 'Multiple features for ' + element.cls
     return result
 
-doc = folia.Document(file='../data/out/T1_VOWYQ147140.xml')
 
-with open('../data/out/comments.csv', 'wb') as csv_file:
-    csv_file.write(u'\uFEFF'.encode('utf-8'))  # the UTF-8 BOM to hint Excel we are using that...
-    csv_writer = UnicodeWriter(csv_file, delimiter=';')
-    header = ['tekstnummer', 'zin nr', 'geannoteerde passage', 'correctie', 'eenheid', 'probleem', 'woordsoort']
-    csv_writer.writerow(header)
+def get_corrections(element, doc_id, sentence_nr):
+    result = []
+    for correction in element.select(folia.Correction, recursive=False):
+        problem = get_feature(correction, 'problem')
+        pos = get_feature(correction, 'pos')
+        original = correction.text(correctionhandling=folia.CorrectionHandling.ORIGINAL)
+        corrected = correction.text(correctionhandling=folia.CorrectionHandling.CURRENT)
+        result.append([doc_id, sentence_nr, original, corrected, correction.cls, problem, pos])
+    return result
 
-    for sentence in doc.sentences():
-        sentence_nr = sentence.id.split('.')[-1]
-        for correction in sentence.select(folia.Correction, recursive=False):
-            problem = get_feature(correction, 'problem')
-            pos = get_feature(correction, 'pos')
-            original = correction.text(correctionhandling=folia.CorrectionHandling.ORIGINAL)
-            corrected = correction.text(correctionhandling=folia.CorrectionHandling.CURRENT)
-            csv_writer.writerow([doc.id, sentence_nr, original, corrected, correction.cls, problem, pos])
-        for word in sentence.words():
-            for correction in word.select(folia.Correction, recursive=False):
-                problem = get_feature(correction, 'problem')
-                pos = get_feature(correction, 'pos')
-                original = correction.text(correctionhandling=folia.CorrectionHandling.ORIGINAL)
-                corrected = correction.text(correctionhandling=folia.CorrectionHandling.CURRENT)
-                csv_writer.writerow([doc.id, sentence_nr, original, corrected, correction.cls, problem, pos])
-        for semrole in sentence.select(folia.SemanticRole):
-            problem = get_feature(semrole, 'problem')
-            pos = get_feature(semrole, 'pos')
-            csv_writer.writerow([doc.id, sentence_nr, semrole.text(), '-', semrole.cls, problem, pos])
+
+def process(filename):
+    doc = folia.Document(file=filename)
+    pre, _ = os.path.splitext(filename)
+    with open(pre + '.csv', 'wb') as csv_file:
+        csv_file.write(u'\uFEFF'.encode('utf-8'))  # the UTF-8 BOM to hint Excel we are using that...
+        csv_writer = UnicodeWriter(csv_file, delimiter=';')
+        header = ['tekstnummer', 'zin nr', 'geannoteerde passage', 'correctie', 'eenheid', 'probleem', 'woordsoort']
+        csv_writer.writerow(header)
+
+        for sentence in doc.sentences():
+            sentence_nr = sentence.id.split('.')[-1]
+
+            # Add Corrections on Sentence and Word level
+            csv_writer.writerows(get_corrections(sentence, doc.id, sentence_nr))
+            for word in sentence.words():
+                csv_writer.writerows(get_corrections(word, doc.id, sentence_nr))
+
+            # Add SemanticRoles
+            for semrole in sentence.select(folia.SemanticRole):
+                problem = get_feature(semrole, 'problem')
+                pos = get_feature(semrole, 'pos')
+                csv_writer.writerow([doc.id, sentence_nr, semrole.text(), '', semrole.cls, problem, pos])
+
+if __name__ == '__main__':
+    for filename in glob.glob('../data/out/*.xml'):
+        process(filename)
