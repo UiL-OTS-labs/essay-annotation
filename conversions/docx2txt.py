9bc242c4567c9077b3c76b99bb21cedd030f0488
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2016-12-12 09:30:25 +0100

    Adding an ArgumentParser

--- conversions/docx2txt.py
+++ conversions/docx2txt.py
@@ -1,13 +1,21 @@
+import argparse
 import codecs
 
 from docx import Document
 
-document = Document('../data/test_errors.docx')
-for table in document.tables:
-    filename = table.rows[0].cells[0].text.strip().split(' ')[0]
-    with codecs.open('../data/' + filename + '.txt', 'wb') as f:
-        for i, row in enumerate(table.rows):
-            for j, cell in enumerate(row.cells):
-                if i != 0 and j == 0:
-                    f.write(cell.text.replace('\n', ' ').encode('utf8'))
-                    f.write('\n')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Converts .docx-documents to individual .txt-files.')
+    parser.add_argument('filenames', type=str, nargs='+', help='The .docx-files to be processed')
+    args = parser.parse_args()
+
+    for f in args.filenames:
+        document = Document(f)
+        for table in document.tables:
+            filename = table.rows[0].cells[0].text.strip().split(' ')[0]
+            with codecs.open('../data/' + filename + '.txt', 'wb') as f:
+                for i, row in enumerate(table.rows):
+                    for j, cell in enumerate(row.cells):
+                        if i != 0 and j == 0:
+                            f.write(cell.text.replace('\n', ' ').encode('utf8'))
+                            f.write('\n')
